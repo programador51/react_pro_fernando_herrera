381810d797e595714078dcaa499f5aae61ca43d5
programador51 <gskitcat@gmail.com>
2021-12-31 21:16:46 -0600

    1.12 Actions with functions on the reducer

--- 01 - reinforcements/src/reducers/counter/CounterReducer.tsx
+++ 01 - reinforcements/src/reducers/counter/CounterReducer.tsx
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import { State } from "reducers/counter/interfaces/interfaces";
 import counterReducer from "reducers/counter/reducer/reducer";
+import { doIncreaseBy, doReset } from "reducers/counter/actions/actions";
 
 interface Props {
   /**
@@ -18,28 +19,14 @@ const INITIAL_STATE: State = {
 export default function CounterComponent({ initialValue = 0 }: Props) {
   const [{ counter }, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
-  const increment = () =>
-    dispatch({
-      payload: {
-        value: 1,
-      },
-      type: "increaseBy",
-    });
+  const increment = () => dispatch(doIncreaseBy(1));
 
   return (
     <>
       <h1>Counter reducer refactored {counter}</h1>
 
       <button onClick={increment}>+1</button>
-      <button
-        onClick={() =>
-          dispatch({
-            type: "reset",
-          })
-        }
-      >
-        Reiniciar
-      </button>
+      <button onClick={() => dispatch(doReset())}>Reiniciar</button>
     </>
   );
 }
