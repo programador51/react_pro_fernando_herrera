697521cf90e60cde27def18e14f7c5398af66c88
programador51 <gskitcat@gmail.com>
2022-01-01 13:39:24 -0600

    3.3 Compount Component Pattern
    Passing props to the component, nice, but not very customizable in the future

--- 02 - rcapp/src/02-component-patterns/pages/ProductCard.tsx
+++ 02 - rcapp/src/02-component-patterns/pages/ProductCard.tsx
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 import styles from "../styles/styles.module.css";
 import useProduct from "02-component-patterns/hooks/useProduct";
 
-export default function ProductCard() {
-  // const [counter, setCounter] = useState(0);
+interface Props {
+  product: Product;
+}
 
-  // const increase = () => setCounter(counter + 1);
-  // const decrease = () => setCounter(counter - 1 <= 0 ? 0 : counter - 1);
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
 
+export default function ProductCard({ product }: Props) {
   const { counter, decrease, increase } = useProduct();
+
+  console.log(product.img);
+
+  const img =
+    product.img !== undefined
+      ? product.img
+      : `${process.env.PUBLIC_URL}/images/no-image.jpg`;
 
   return (
     <div className={styles.productCard}>
       <img
         className={styles.productImg}
-        src={`${process.env.PUBLIC_URL}/images/coffee-mug.png`}
+        src={img}
         alt="mug of the product to sell"
       />
-      <span className={styles.productDescription}>Coffee mug</span>
+      <span className={styles.productDescription}>{product.title}</span>
 
       <div className={styles.buttonsContainer}>
         <button onClick={decrease} className={styles.buttonMinus}>
--- 02 - rcapp/src/02-component-patterns/pages/Shopping.tsx
+++ 02 - rcapp/src/02-component-patterns/pages/Shopping.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ProductCard from "02-component-patterns/pages/ProductCard";
-import styles from "02-component-patterns/styles/styles.module.css";
+// import styles from "02-component-patterns/styles/styles.module.css";
+
+const product = {};
 
 export default function Shopping() {
   return (
     <div>
-      Shopping page <hr />
-      <ProductCard />
+      <h1>Shopping page </h1>
+      <hr />
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        <ProductCard
+          product={{
+            id: "1",
+            title: "123",
+            // img: "12312",
+          }}
+        />
+        {/* <ProductCard /> */}
+        {/* <ProductCard /> */}
+        {/* <ProductCard /> */}
+        {/* <ProductCard /> */}
+      </div>
     </div>
   );
 }
