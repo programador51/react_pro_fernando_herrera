10673177adce68cff51ee60e42f83389e89c60fb
programador51 <gskitcat@gmail.com>
2022-01-08 15:02:13 -0600

    8.7 Formik - Basics
    Configuration to set values on inputs and handle the submit (uncontrolled components)

--- 03-render-props/src/pages/FormikBasic.tsx
+++ 03-render-props/src/pages/FormikBasic.tsx
@@ -1,21 +1,51 @@
 import React from "react";
+import { useFormik } from "formik";
 import "styles/styles.css";
 
 export default function FormikBasic() {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      name: "Test",
+      lastname: "",
+      email: "",
+    },
+    onSubmit: (data) => {
+      console.log(data);
+    },
+  });
+
   return (
     <div>
       <h1>Formik tutorial basic</h1>
-      <form noValidate>
+      <form onSubmit={handleSubmit} noValidate>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          id="name"
+          value={values.name}
+        />
         <span>Required</span>
 
         <label htmlFor="lastname">Lastname</label>
-        <input type="text" name="lastname" id="lastname" />
+        <input
+          onChange={handleChange}
+          value={values.lastname}
+          type="text"
+          name="lastname"
+          id="lastname"
+        />
         <span>Required</span>
 
         <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
+        <input
+          onChange={handleChange}
+          value={values.email}
+          type="text"
+          name="email"
+          id="email"
+        />
         <span>Required</span>
         <span>Invalid email</span>
 
