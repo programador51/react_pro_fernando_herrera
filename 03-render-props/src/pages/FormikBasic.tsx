843d9fe8c5cf87a61973ec8aeae1b33059eeb61d
programador51 <gskitcat@gmail.com>
2022-01-08 16:06:17 -0600

    8.8 Formik - Basics (manual validation)
    Using the custom hooks of the library and validating manually, but just once must be created the validation code

--- 03-render-props/src/pages/FormikBasic.tsx
+++ 03-render-props/src/pages/FormikBasic.tsx
@@ -1,17 +1,49 @@
 import React from "react";
-import { useFormik } from "formik";
+import { useFormik, FormikErrors } from "formik";
 import "styles/styles.css";
+import { error } from "console";
+
+export interface RegisterI {
+  name: string;
+  lastname: string;
+  email: string;
+}
 
 export default function FormikBasic() {
+  const validate = ({ email, lastname, name }: RegisterI) => {
+    const errors: FormikErrors<RegisterI> = {};
+
+    if (!name) {
+      errors.name = "Required";
+    } else if (name.length > 15) {
+      errors.name = "15 or less characters allowed";
+    }
+
+    if (!lastname) {
+      errors.lastname = "Required";
+    } else if (name.length > 20) {
+      errors.lastname = "20 or less characters allowed";
+    }
+
+    if (!email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      errors.email = "Invalid email address";
+    }
+
+    return errors;
+  };
+
   const { handleChange, values, handleSubmit } = useFormik({
     initialValues: {
       name: "Test",
       lastname: "",
       email: "",
     },
-    onSubmit: (data) => {
+    onSubmit: (data: RegisterI) => {
       console.log(data);
     },
+    validate,
   });
 
   return (
