9396b1e8621da458dc6c8e0a7379378c48d996f1
programador51 <gskitcat@gmail.com>
2022-01-08 13:18:40 -0600

    8.2 Formik - useState
    Using useState hook to handle the traditional form

--- 03-render-props/src/pages/RegisterPage.tsx
+++ 03-render-props/src/pages/RegisterPage.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import "styles/styles.css";
 
 export default function RegisterPage() {
+  const [registerData, setRegisterData] = useState({
+    name: "jose",
+    email: "programa51@gmail.com",
+    password2: "123",
+    password1: "123",
+  });
+
+  const { email, name, password1, password2 } = registerData;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <h1>Register page</h1>
       <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password1" placeholder="Password" />
-        <input type="password2" placeholder="Repeat password" />
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password1"
+          placeholder="Password"
+          value={password1}
+          onChange={onChange}
+        />
+        <input
+          type="password2"
+          placeholder="Repeat password"
+          value={password2}
+          onChange={onChange}
+        />
         <input type="submit" value="create" />
       </form>
     </div>
