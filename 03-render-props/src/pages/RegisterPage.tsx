8e48d7679c8a56534472e7e03d17f642470dbe28
programador51 <gskitcat@gmail.com>
2022-01-08 13:43:08 -0600

    8.3 Formik - updatingValues
    Using the set state to update information when user it's using the form

--- 03-render-props/src/pages/RegisterPage.tsx
+++ 03-render-props/src/pages/RegisterPage.tsx
@@ -1,45 +1,56 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import "styles/styles.css";
 
 export default function RegisterPage() {
   const [registerData, setRegisterData] = useState({
-    name: "jose",
-    email: "programa51@gmail.com",
-    password2: "123",
-    password1: "123",
+    name: "",
+    email: "",
+    password2: "",
+    password1: "",
   });
 
   const { email, name, password1, password2 } = registerData;
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+    setRegisterData({
+      ...registerData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
   };
 
   return (
     <div>
       <h1>Register page</h1>
-      <form>
+      <form noValidate onSubmit={(e) => onSubmit(e)}>
         <input
           type="text"
           placeholder="Name"
           value={name}
+          name="name"
           onChange={(e) => onChange(e)}
         />
         <input
           type="email"
           placeholder="Email"
+          name="email"
           value={email}
           onChange={onChange}
         />
         <input
           type="password1"
           placeholder="Password"
+          name="password1"
           value={password1}
           onChange={onChange}
         />
         <input
           type="password2"
           placeholder="Repeat password"
+          name="password2"
           value={password2}
           onChange={onChange}
         />
