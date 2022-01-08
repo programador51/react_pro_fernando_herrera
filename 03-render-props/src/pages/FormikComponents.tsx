02afac067ea5553959d8f81f3021b8fba4c70f84
programador51 <gskitcat@gmail.com>
2022-01-08 17:51:52 -0600

    8.13 Formik - Basics (Field FC)
    Using that FC to render a select option and other stuff

--- 03-render-props/src/pages/FormikComponents.tsx
+++ 03-render-props/src/pages/FormikComponents.tsx
@@ -19,6 +19,8 @@ export default function FormikComponents() {
           name: "",
           lastname: "",
           email: "",
+          terms: false,
+          jobType: 2,
         }}
         validationSchema={yup.object({
           name: yup.string().max(15, "Max 15 characters").required("Required"),
@@ -27,6 +29,10 @@ export default function FormikComponents() {
             .max(15, "Max 15 characters")
             .required("Required"),
           email: yup.string().email("Invalid email").required("Required"),
+          terms: yup
+            .boolean()
+            .oneOf([true], "Must accept terms and conditions"),
+          jobType: yup.number().transform((castedVal, inputVal) => +castedVal),
         })}
         onSubmit={(values) => console.log(values)}
       >
@@ -43,6 +49,19 @@ export default function FormikComponents() {
             <label htmlFor="email">Email</label>
             <Field name="email" type="text" />
             <ErrorMessage name="email" component="span" />
+
+            <label>
+              <Field name="terms" type="checkbox" /> Terms and conditions
+            </label>
+            <ErrorMessage name="terms" component="span" />
+
+            <label>Job type</label>
+            <Field name="jobType" type="number" as="select">
+              <option value={0}>Pick something</option>
+              <option value={1}>Dev Ops</option>
+              <option value={2}>Backend</option>
+            </Field>
+            <ErrorMessage name="jobType" component="span" />
 
             <input type="submit" value="Save" />
           </Form>
